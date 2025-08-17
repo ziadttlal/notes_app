30119c87ecc88e7590277d3c31fcd7259b08ca31
ziad ttlal <ziadttlal8@gmail.com>
2025-08-17 09:26:19 +0300

    create note model

---
+++ lib/models/not_model.dart
@@ -0,0 +1,13 @@
+class NotModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  NotModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+}
