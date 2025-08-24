9c1e21b1829ffe663c434ff0eeadded0096f77a1
ziad ttlal <ziadttlal8@gmail.com>
2025-08-24 23:52:42 +0300

    create notes state and create fetch notes methods

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
@@ -7,22 +9,26 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(right: 8),
-        child: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-              context: context,
-              builder: (context) {
-                return AddNoteBottomSheet();
-              },
-            );
-          },
-          child: Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(right: 8),
+          child: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
+                  return AddNoteBottomSheet();
+                },
+              );
+            },
+            child: Icon(Icons.add),
+          ),
         ),
+        body: NotesViewBody(),
       ),
-      body: NotesViewBody(),
     );
   }
 }
--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -25,7 +25,12 @@ class AddNoteBottomSheet extends StatelessWidget {
           builder: (context, state) {
             return AbsorbPointer(
               absorbing: state is AddNoteLoading ? true : false,
-              child: SingleChildScrollView(child: AddNoteForm()),
+              child: Padding(
+                padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom,
+                ),
+                child: SingleChildScrollView(child: AddNoteForm()),
+              ),
             );
           },
         ),
